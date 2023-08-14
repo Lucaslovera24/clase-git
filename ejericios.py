3faffbbc649a51e7b9229ae78015b35858d92fb8
Lucas Lovera <Lucaslovera24@hotmail.com>
2023-08-14 09:25:48 -0300

    Cambio ejercicio 1

--- ejericios.py
+++ ejericios.py
@@ -3,10 +3,11 @@ Asigne un mensaje a una variable e imprima ese mensaje. Luego cambie el valor de
 variable a un nuevo mensaje e imprima el nuevo mensaje.
 '''
 var1 = "Hola mundo"
-mensaje = f"El valor de la variable es {var1}"
-print(mensaje)
+print(f"El valor de la variable es {var1}")
 
 '''
 Use una variable para representar el nombre de una persona e imprima el mensaje "Hola
 <nombre de la persona>,¿te gustaría aprender algo de Python hoy?".
 '''
+nombre = "Lucas"
+print(f"Hola {nombre},¿te gustaria aprender algo de python hoy?")
